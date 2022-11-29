f9a4ae5f0e834a10cabe5939cb248fbfbf8972d1
Triston Armstrong <triston95strong@gmail.com>
2022-11-29 09:15:44 -0600

    add hooks fixing unresovable external modules

--- forge.config.ts
+++ forge.config.ts
@@ -4,6 +4,9 @@ import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import fs from 'fs-extra'
+import path from 'path'
+import {spawn} from 'child_process'
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -30,6 +33,44 @@ const config: ForgeConfig = {
       },
     }),
   ],
+  hooks: {
+    readPackageJson: async (forgeConfig, packageJson) => {
+      // https://github.com/electron/forge/issues/1250#issuecomment-667543166
+      // https://github.com/electron/forge/pull/2345
+      // https://www.npmjs.com/package/@timfish/forge-externals-plugin
+      
+      // only copy deps if there isn't any
+      if (Object.keys(packageJson.dependencies).length === 0) {
+        const originalPackageJson = await fs.readJson(path.resolve(__dirname, 'package.json'));
+        const webpackConfigJs = await require('./webpack.renderer.config.js');
+        Object.keys(webpackConfigJs.externals).forEach(_package => {
+          packageJson.dependencies[_package] = originalPackageJson.dependencies[_package];
+        });
+      }
+      return packageJson;
+    },
+    packageAfterPrune: async (forgeConfig, buildPath) => {
+      console.log(buildPath);
+      return new Promise((resolve, reject) => {
+        const npmInstall = spawn('npm', ['install'], {
+          cwd: buildPath,
+          stdio: 'inherit',
+        });
+
+        npmInstall.on('close', (code) => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject(new Error('process finished with error code ' + code));
+          }
+        });
+
+        npmInstall.on('error', (error) => {
+          reject(error);
+        });
+      });
+    }
+  }
 };
 
 export default config;
